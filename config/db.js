95dc0f4ef8c6854cc61f6bda083ce1667f98650e
rajat4rao <balprao@gmail.com>
2024-10-28 15:09:44 +0530

    init

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+
+const mongoose = require("mongoose");
+dotenv.config();
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
